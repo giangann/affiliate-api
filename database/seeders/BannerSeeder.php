11f788e4b0683695d5b052fefbe3a21247e74088
giangann <70595717+giangann@users.noreply.github.com>
2022-09-18 02:31:19 +0700

    update seeder

--- database/seeders/BannerSeeder.php
+++ database/seeders/BannerSeeder.php
@@ -17,24 +17,43 @@ class BannerSeeder extends Seeder
         DB::table('banners')->insert([
             [
                 'link_of_image'=>'https://apimg.net/slider/adsterracpa-vpncasestudy-546x234.jpg',
-                'path_to_website'=>'https://adsterra.com/cpanetwork/?utm_source=affpaying&utm_medium=pr&utm_campaign=cpa_affiliate_case_0922&utm_content=affiliate_case0922'
+                'path_to_website'=>'https://adsterra.com/cpanetwork/?utm_source=affpaying&utm_medium=pr&utm_campaign=cpa_affiliate_case_0922&utm_content=affiliate_case0922',
+                'type'=>1
             ],
             [
                 'link_of_image'=>'https://apimg.net/slider/highrockads-slider.png',
-                'path_to_website'=>'https://publishers.highrockads.com/v2/sign/up'
+                'path_to_website'=>'https://publishers.highrockads.com/v2/sign/up',
+                'type'=>1
             ],
             [
                 'link_of_image'=>'https://apimg.net/slider/1xbetpartners546x234.jpg',
-                'path_to_website'=>'https://1xpartners.com/sign-up?tag=d_1427065m_2387c_subaffart'
+                'path_to_website'=>'https://1xpartners.com/sign-up?tag=d_1427065m_2387c_subaffart',
+                'type'=>1
             ],
             [
                 'link_of_image'=>'https://apimg.net/slider/howto-setup-tracker-mylead-546x234.jpg',
-                'path_to_website'=>'https://mylead.global/en/register?utm_source=affpaying.com&utm_medium=Content-marketing&utm_campaign=wsp%C3%B3%C5%82praca-artyku%C5%82&utm_content=EN'
+                'path_to_website'=>'https://mylead.global/en/register?utm_source=affpaying.com&utm_medium=Content-marketing&utm_campaign=wsp%C3%B3%C5%82praca-artyku%C5%82&utm_content=EN',
+                'type'=>1
             ],
             [
                 'link_of_image'=>'https://apimg.net/slider/n1-partners-2022-2.jpg',
-                'path_to_website'=>'https://n1.partners/?utm_campaign=Affpaying&utm_medium=N1_Partners&utm_source=affil_prog'
+                'path_to_website'=>'https://n1.partners/?utm_campaign=Affpaying&utm_medium=N1_Partners&utm_source=affil_prog',
+                'type'=>1
             ],
+            [
+                'link_of_image'=>'https://apimg.net/2020/Affpaying%20125x125.gif',
+                'path_to_website'=>'https://evadav.com/',
+                'type'=>3
+            ],[
+                'link_of_image'=>'https://apimg.net/2020/affmine-125x125.gif',
+                'path_to_website'=>'https://www.affmine.com/?utm_source=affpaying&utm_medium=banner',
+                'type'=>3
+            ],[
+                'link_of_image'=>'https://apimg.net/2022/lemonad_easy_peasy_300x250.gif',
+                'path_to_website'=>'https://limonad.com/en/?utm_source=affpaying2',
+                'type'=>2
+            ],
+
         ]);
     }
 }
